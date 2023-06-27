8fd43fb012897fdb83ec8bf430c60936dec27eca
Ray KST <jailma.kmp@gmail.com>
2023-06-27 19:54:55 +0000

    [fix] pages redirects

---
+++ static/controllers/indexController.js
@@ -0,0 +1,13 @@
+// document.body.querySelectorAll('li').forEach((li)=>{
+//     console.log(li.textContent)
+// })
+
+// document.body.querySelectorAll('li').forEach((li)=>{
+//     li.addEventListener("click", ()=>{
+//       if (li.textContent == 'Times'){
+//         document.location.href = '/mostrar-times'
+//       }else{
+//         document.location.href = '/mostrar-times'
+//       }
+//     })
+//   })
